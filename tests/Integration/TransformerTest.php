5dc5a3922f1ea0af55487579e13fdada303c2e73
Josh Forbes <jforbes@kimmel.com>
2015-12-28 11:24:13 -0500

    Adding tests to check that transformers properly validate passed parameters.

--- tests/Integration/TransformerTest.php
+++ tests/Integration/TransformerTest.php
@@ -2,12 +2,14 @@
 
 namespace NavJobs\LaravelApi\Test\Integration;
 
-class EagerLoadTest extends TestCase
+use League\Fractal\ParamBag;
+
+class TransformerTest extends TestCase
 {
     /**
  * @test
  */
-    public function it_correctly_parses_a_string_of_includes()
+    public function it_correctly_parses_a_string_of_eager_load_includes()
     {
         $transformer = new TestTransformer();
         $array = $transformer->getEagerLoads('characters,books,publisher');
@@ -23,7 +25,7 @@ class EagerLoadTest extends TestCase
     /**
      * @test
      */
-    public function it_correctly_parses_an_array_of_includes()
+    public function it_correctly_parses_an_array_of_eager_load_includes()
     {
         $transformer = new TestTransformer();
         $array = $transformer->getEagerLoads(['characters', 'books', 'publisher']);
@@ -39,12 +41,44 @@ class EagerLoadTest extends TestCase
     /**
      * @test
      */
-    public function it_returns_nothing_when_no_matches()
+    public function it_returns_nothing_when_no_eager_load_matches()
     {
         $transformer = new TestTransformer();
         $array = $transformer->getEagerLoads('papers, books,staplers');
         $expectedArray = [];
 
         $this->assertEquals($expectedArray, $array);
+    }
+
+    /**
+     * @test
+     */
+    public function it_accepts_valid_parameters()
+    {
+        $transformer = new TestTransformer();
+        $valid = $transformer->validateParameters(new ParamBag([
+            'limit' => [
+                '1' => '1'
+            ],
+            'order' => [
+                'created_at' => 'desc'
+            ]
+        ]));
+
+        $this->assertEquals(true, $valid);
+    }
+
+    /**
+     * @test
+     * @expectedException NavJobs\LaravelApi\Exceptions\InvalidParameters
+     */
+    public function it_throws_an_exception_for_invalid_parameters()
+    {
+        $transformer = new TestTransformer();
+        $transformer->validateParameters(new ParamBag([
+            'not-valid' => [
+                '1' => '1'
+            ]
+        ]));
     }
 }
