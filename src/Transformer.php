f16617d01f493a3abc9fbe0b7564acff372654da
Josh Forbes <jforbes@kimmel.com>
2015-12-28 11:24:51 -0500

    Add valid parameters array and the validate parameters method to the base transformer.

--- src/Transformer.php
+++ src/Transformer.php
@@ -2,10 +2,40 @@
 
 namespace NavJobs\LaravelApi;
 
+use League\Fractal\ParamBag;
 use League\Fractal\TransformerAbstract;
+use NavJobs\LaravelApi\Exceptions\InvalidParameters;
 
-class Transformer extends TransformerAbstract
+abstract class Transformer extends TransformerAbstract
 {
+    /**
+     * An array of allowed parameters.
+     *
+     * @var array
+     */
+    protected $validParameters;
+
+    /**
+     * Checks if the provided parameters are valid.
+     *
+     * @param ParamBag $parameters
+     * @return bool
+     * @throws InvalidParameters
+     */
+    public function validateParameters(ParamBag $parameters)
+    {
+        $usedParameters = array_keys(iterator_to_array($parameters));
+        if ($invalidParams = array_diff($usedParameters, $this->validParameters)) {
+            throw new InvalidParameters(
+                sprintf('Invalid param(s): "%s". Valid param(s): "%s"',
+                    implode(',', $usedParameters),
+                    implode(',', $this->validParameters)
+                ));
+        }
+
+        return true;
+    }
+
     /**
      * Returns the includes that are available for eager loading.
      *
