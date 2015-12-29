29686d6c4b4d2b661d890ca66269c69964c0c469
Josh Forbes <jforbes@kimmel.com>
2015-12-29 10:25:30 -0500

    Adding a test that verifies the transformer applies the parameters to the include.

--- tests/Integration/TransformerTest.php
+++ tests/Integration/TransformerTest.php
@@ -79,8 +79,33 @@ class TransformerTest extends TestCase
         $transformer = new TestTransformer();
         $transformer->validateParameters(new ParamBag([
             'not-valid' => [
-                '1' => '1'
+                0 => '1'
             ]
         ]));
+    }
+
+    /**
+     * @test
+     */
+    public function it_applies_parameters_to_a_query_builder()
+    {
+        $builder = new TestBuilder();
+
+        $transformer = new TestTransformer();
+        $builder = $transformer->applyParameters($builder, new ParamBag([
+            'limit' => [
+                0 => '5',
+                1 => '2'
+            ],
+            'sort' => [
+                0 => 'created_at',
+                1 => 'desc'
+            ]
+        ]));
+
+        $this->assertEquals(5, $builder->getLimit());
+        $this->assertEquals(2, $builder->getOffset());
+        $this->assertEquals('created_at', $builder->getSort());
+        $this->assertEquals('desc', $builder->getOrder());
     }
 }
