dc5d4327aa45565eb17f4f6b3a2606e04f273b34
Zach Silveira <zackify@gmail.com>
2017-02-13 16:08:39 -0500

    Add Resource Template

---
+++ src/Templates/ResourceController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace NavJobs\Transmit\Templates;
+
+use NavJobs\Transmit\Controller;
+
+class CourseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return $this->respondWithPaginatedCollection($model);
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store()
+    {
+        $item = $this->model->create(request()->all());
+        return $this->respondWithItem($item);
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return $this->respondWithItem($this->model, function ($model) use ($id) {
+            return $model->findOrFail($id);
+        });
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update($id, Request $request)
+    {
+        return $this->respondWithItem($this->model, function ($model) use ($id, $request) {
+            $item = $model->findOrFail($id);
+            $item->fill($request->all());
+            $item->save();
+            return $item;
+        });
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $this->model->findOrFail($id)->delete();
+        return $this->respondWithNoContent();
+    }
+}
